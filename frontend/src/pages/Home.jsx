eabc1be8e7124d12e0d98e57a099bd3b16c4d47f
Marc LANTOL <marc.lantol@gmail.com>
2022-07-20 16:35:16 +0200

    Homepage canvas

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,24 @@
+import Menu from "../components/Menu";
+import "../styles/Home.scss";
+
 export default function Home() {
-  return <div>test</div>;
+  return (
+    <div className="home-container">
+      <Menu />
+      <div className="container">
+        <p className="title">TITLE</p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis,
+          corrupti odio aliquid cumque aperiam inventore nisi aut beatae. Facere
+          dicta deleniti aliquid dolor nobis quo numquam! Minima, placeat
+          mollitia? Corporis. Cumque aut odit eum necessitatibus delectus eius
+          doloremque? Non explicabo excepturi, maxime ipsa, dignissimos beatae
+          voluptatum sequi vero quo alias rem nulla quasi? Numquam fuga
+          consequuntur sequi facilis cumque corrupti! Illum quia natus sit
+          ipsum. Voluptas facilis voluptates ea fugiat dolorem expedita odit,
+          consequatur eius beatae maiores odio atque. Numquam quos officia modi
+        </p>
+      </div>
+    </div>
+  );
 }
