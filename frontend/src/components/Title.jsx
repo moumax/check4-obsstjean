4db41939b3d6ccf74f807ff81a59516fd625febb
Marc LANTOL <marc.lantol@gmail.com>
2022-07-21 00:55:19 +0200

    Desktop Home is working

---
+++ frontend/src/components/Title.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../styles/Title.scss";
+
+export default function Title() {
+  return (
+    <div className="title">
+      {" "}
+      <h1>Observatoire Astronomique de Saint Jean Le Blanc</h1>
+      <h6>Association loi 1901 pour la promotion de l'astronomie amateur</h6>
+    </div>
+  );
+}
