3f1a913fb50cb54d8f8c5249a7bed8aaa8f52db2
Marc LANTOL <marc.lantol@gmail.com>
2022-07-21 16:45:40 +0200

    Desktop is ok

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,38 +1,34 @@
+import { useState, useEffect } from "react";
+import userAPI from "../services/userAPI";
 import "../styles/Events.scss";
 
 export default function Events() {
+  const [event, setEvent] = useState([]);
+  const getEvent = () => {
+    userAPI.get("/api/event").then((response) => {
+      setEvent(response.data[0]);
+    });
+  };
+
+  useEffect(() => {
+    getEvent();
+  }, []);
+
   return (
     <div className="events-container">
       <div className="container">
-        <p className="title">Events</p>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis,
-          corrupti odio aliquid cumque aperiam inventore nisi aut beatae. Facere
-          dicta deleniti aliquid dolor nobis quo numquam! Minima, placeat
-          mollitia? Corporis. Cumque aut odit eum necessitatibus delectus eius
-          doloremque? Non explicabo excepturi, maxime ipsa, dignissimos beatae
-          voluptatum sequi vero quo alias rem nulla quasi? Numquam fuga
-          consequuntur sequi facilis cumque corrupti! Illum quia natus sit
-          ipsum. Voluptas facilis voluptates ea fugiat dolorem expedita odit,
-          consequatur eius beatae maiores odio atque. Numquam quos officia modi
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id at hic
-          aliquid dignissimos mollitia, nam, eveniet adipisci laborum earum odio
-          ipsam vel doloribus. Deleniti ducimus atque illo earum quasi fugiat.
-          Sit corporis libero voluptates molestiae officia fugiat ut tempora
-          perferendis debitis quidem ipsum accusantium harum dolore sint, nam
-          enim repudiandae. Voluptatem ea assumenda provident enim pariatur
-          sequi ut odio officiis! Rerum numquam culpa velit molestias tempore
-          ipsum architecto quidem porro explicabo saepe sed nostrum, aliquid
-          modi? Architecto animi eius saepe tempora cupiditate nobis, eveniet
-          ullam exercitationem laboriosam cum placeat unde. Alias vitae velit
-          molestias corporis nam quas delectus quidem dignissimos ea voluptates.
-          Sapiente eligendi beatae vitae est ipsum esse mollitia laudantium!
-          Corporis, molestiae sint? Reprehenderit quas sequi tempora accusantium
-          dolore! Iusto, quibusdam id quo ea rem modi voluptates repudiandae
-          voluptas, culpa expedita, qui ex delectus? Repellat laboriosam
-          obcaecati itaque excepturi natus ea illum, laborum perferendis eaque
-          veritatis, rem blanditiis veniam.
-        </p>
+        <h1 className="title">Events</h1>
+        <div className="timeline">
+          {event.map((data) => (
+            <ul>
+              <li className="date" data-date={data.date}>
+                <h2>{data.title}</h2>
+                <p>{data.description}</p>
+                <p>{data.site}</p>
+              </li>
+            </ul>
+          ))}
+        </div>
       </div>
     </div>
   );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,21 @@
+import { useNavigate } from "react-router-dom";
 import Observatoire from "../components/Observatoire";
 import Title from "../components/Title";
 import "../styles/Home.scss";
 
 export default function Home() {
+  const navigate = useNavigate();
   return (
     <div className="home-container">
       <div className="container">
         <Title />
         <Observatoire />
+        <button type="button" onClick={() => navigate("/events")}>
+          x
+        </button>
+        <button type="button" onClick={() => navigate("/events")}>
+          o
+        </button>
       </div>
     </div>
   );
