bbb2f8d858e5e6b09cae50ccffa1f650f06bc43a
Marc LANTOL <marc.lantol@gmail.com>
2022-07-22 12:49:22 +0200

    hotfix

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,5 +1,20 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import "../styles/Error.scss";
 
 export default function Error() {
-  return <div>Error</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="home-container">
+      <img src="src/assets/pika.png" alt="Pikachu pas content"></img>
+      <p className="err-text">
+        Ceci est une page d'erreur incroyablement bien stylisée !!!!!!
+      </p>
+      <button className="btn" type="submit" onClick={() => navigate("/")}>
+        RETOUR
+      </button>
+    </div>
+  );
 }
